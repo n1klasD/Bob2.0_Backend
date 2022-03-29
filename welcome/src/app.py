70f18d15b50cc3eb42662ad4cc8ade64f173af59
Tom-Luis Vince <inf19022@lehre.dhbw-stuttgart.de>
2022-03-29 19:43:39 +0200

    base for welcome dialog
    defined routes, added first messages (examples), added rapla info

--- welcome/src/app.py
+++ welcome/src/app.py
@@ -1,13 +1,30 @@
 from flask import Flask
+import datasources
 
 app = Flask(__name__)
 
+
 @app.route('/')
+def index():
+    return "Dies ist der Welcome Dialog"
 
-def hello():
-    
-    return "Hello from Welcome dialog"
+@app.route('/briefing')
+def briefing():
+    userName="Max Mustermann"
+    return f"Guten Morgen {userName}. Heute steht ein schöner Tag bevor. Dein erster Termin ist um ..., gefolgt von ... danach eine Pause bis ..., danach sind deine geplanten Termine erledigt. Was du vielleicht verpasst hast: _Nachrichten API_. Und vergiss nicht: _motivational quotes API_."
 
+@app.route('/wetter')
+def weather():
+    return "heute ist ein schoener Tag."
+
+@app.route('/termine')
+def meetings():
+    return "Heute hast du keine Termine."
+
+@app.route('/stundenplan')
+def timetable():
+    answer = datasources.get_rapla_data()
+    return answer
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
 
---
+++ welcome/src/datasources.py
@@ -0,0 +1,59 @@
+import datetime
+from bs4 import BeautifulSoup
+import requests
+
+
+def get_rapla_data():
+    date ="today" 
+    link = "https://rapla.dhbw-stuttgart.de/rapla?key=txB1FOi5xd1wUJBWuX8lJhGDUgtMSFmnKLgAG_NVMhBUYcX7OIFJ2of49CgyjVbV"
+        
+    if date == "today":
+        now = datetime.datetime.now()
+    elif date == "tomorrow":
+        now = datetime.datetime.now() + datetime.timedelta(days=1)
+    else:
+        # test if the given date is valid
+        try:
+            now = datetime.datetime.strptime(date, "%d.%m.%y")
+        except:
+            
+            return "Invalid Format"
+
+    # add date as url params
+    link += "&day=" + str(now.day)
+    link += "&month=" + str(now.month)
+    link += "&year=" + str(now.year)
+    link += "&today=Heute"
+
+    # integer representation of the day in the week, where 0 is monday and 6 is sunday
+    weekday = now.weekday()
+
+    if weekday > 4:
+        # saturday or sunday
+        
+        return ("Free")
+
+    # get html content and parse it with bs4
+    webpage = requests.get(link)
+    soup = BeautifulSoup(webpage.text, "html.parser")
+    
+    answer =  ""
+
+    weekdays_german = ("Mo", "Di", "Mi", "Do", "Fr")
+    
+    week_blocks = soup.find_all("td", class_ = "week_block")
+    for week_block in week_blocks:
+        # get all days plan in matching week and iterte until the right one is found
+        info = week_block.a.get_text().split("\n")
+        time = info[1].split(" ")
+        if weekdays_german.index(time[0]) != weekday:
+            continue
+        course = info[3]
+        person = info[21]
+        
+        answer += time[1] + ": " + course + " bei " + person[:-1] + "\n"
+
+    return answer
+
+
+    return data.Close
