44d231e6440aea5efa77973ef4e59e1fa0daf949
Tom-Luis Vince <inf19022@lehre.dhbw-stuttgart.de>
2022-03-29 20:13:17 +0200

    changed name for proactive dialog

--- welcome/src/app.py
+++ welcome/src/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     return "Dies ist der Welcome Dialog"
 
-@app.route('/briefing')
+@app.route('/welcome')
 def briefing():
     userName="Max Mustermann"
     return f"Guten Morgen {userName}. Heute steht ein schöner Tag bevor. Dein erster Termin ist um ..., gefolgt von ... danach eine Pause bis ..., danach sind deine geplanten Termine erledigt. Was du vielleicht verpasst hast: _Nachrichten API_. Und vergiss nicht: _motivational quotes API_."
@@ -25,6 +25,6 @@ def meetings():
 def timetable():
     answer = datasources.get_rapla_data()
     return answer
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
-
