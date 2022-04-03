6c1d1995737edca350bc7fb6b993570fc7c7cbbe
n1klasD <67094918+n1klasD@users.noreply.github.com>
2022-04-03 19:21:53 +0200

    Update app.py

--- controller/src/app.py
+++ controller/src/app.py
@@ -27,27 +27,31 @@ usecase2port = {
 def process_text():
     data = request.get_json()
     speech_text = data["speech"]
-    preferences = data["preferences"]
+    # preferences = data["preferences"]
 
     question = SpeechParser.speech2route(speech_text)
-    if question:
-        # Call the matched usecase, and pass the preferences along
-        usecase_response = post_request(question.get_usecase_name(),
-                                        question.get_route(),
-                                        preferences)
-        # todo: Error handling
-        tts = usecase_response.text
-        further_questions = question.get_further_questions(4)
-    else:
-        tts = random.choice(config.no_answer)
-        further_questions = []
+    # if question:
+    # Call the matched usecase, and pass the preferences along
+    # usecase_response = post_request(question.get_usecase_name(),
+    #                               question.get_route(),
+    #                               preferences)
+    # todo: Error handling
+    # tts = usecase_response.text
+    # further_questions = question.get_further_questions(4)
+    # else:
+    #    tts = random.choice(config.no_answer)
+    #     further_questions = []
 
-    response = {
-        "usecase": question.get_usecase_name(),
-        "tts": tts,
-        "further_questions": further_questions
-    }
-    return jsonify(response), 200
+    # response = {
+    #    "usecase": question.get_usecase_name(),
+    #    "tts": tts,
+    #    "further_questions": further_questions
+    # }
+
+    if question:
+        return jsonify({"route": question.get_route()}), 200
+    else:
+        return jsonify({"route": "none"}), 200
 
 
 if __name__ == "__main__":
