7c8ec999f2bf8df882562a882c36fdd044ee6a8e
n1klasD <67094918+n1klasD@users.noreply.github.com>
2022-04-04 10:28:31 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo docker pull 1646552/container:latest
+sudo docker pull 1646552/controller:latest
 sudo docker pull 1646552/welcome:latest
 sudo docker pull 1646552/entertainment:latest
 sudo docker pull 1646552/journey:latest
