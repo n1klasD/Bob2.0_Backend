a95e0abd50e6c90e170d0275690de9a789ae0497
n1klasD <67094918+n1klasD@users.noreply.github.com>
2022-04-04 10:27:45 +0200

    added start script

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+sudo docker pull 1646552/container:latest
+sudo docker pull 1646552/welcome:latest
+sudo docker pull 1646552/entertainment:latest
+sudo docker pull 1646552/journey:latest
+sudo docker pull 1646552/finances:latest
+sudo docker-compose up
