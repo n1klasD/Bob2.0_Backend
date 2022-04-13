febf140a71c4adcc39a517aab48e8e3c414637b7
alexhobden <alex.hobden@gmx.de>
2022-04-13 10:10:22 +0200

    Implementation of Journey Usecase

--- journey/src/app.py
+++ journey/src/app.py
@@ -1,6 +1,6 @@
-from crypt import methods
+#from crypt import methods
 from flask import Flask, request
-from .datasources import get_Gas_Stations_Rad, get_Distance, get_Route
+from . import datasources
 
 app = Flask(__name__)
 
@@ -15,17 +15,17 @@ def index():
 @app.route('/gasStations', methods=["POST"])
 def gasStations():
     data = request.get_json()
-    stations = get_Gas_Stations_Rad(data[home])
+    stations = datasources.get_Gas_Stations_Rad(data[home])
     return stations
 
 @app.route('/distance', methods=["POST"])
 def getdistance():
     data = request.get_json()
-    distances = get_Distance(data[home], data[work])
+    distances = datasources.get_Distance(data[home], data[work])
     return distances
 
-@app.router('/route', methods=["POST"])
+@app.route('/route', methods=["POST"])
 def get_Route():
     data = request.get_json()
-    route = get_Route(data[home], data[work])
+    route = datasources.get_Route(data[home], data[work])
     return route
