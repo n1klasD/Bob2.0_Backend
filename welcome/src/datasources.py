c93e32f909248651a1fb6a9e880a657e301633f7
Tom-Luis Vince <inf19022@lehre.dhbw-stuttgart.de>
2022-04-13 15:00:04 +0200

    vorhaut von news beschnitten

--- welcome/src/datasources.py
+++ welcome/src/datasources.py
@@ -36,7 +36,7 @@ def get_news_data(category):
     elif not 'articles' in text:
         return (f"{category}: Keine Neuigkeiten.")
     text = text['articles'][0]
-    return(f"{category}: {text['title']} (von {text['author']},{text['published_date']}) Hier weiterlesen: {text['link']}")
+    return(f"{category}: {text['title']} ({text['author']}).")
 
 def get_motivational_quote():
     
