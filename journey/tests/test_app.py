20535cc92ac96635a60acbbbcc408de5512457be
alexhobden <alex.hobden@gmx.de>
2022-04-13 19:05:43 +0200

    Tests complete

--- journey/tests/test_app.py
+++ journey/tests/test_app.py
@@ -1,6 +1,6 @@
-from ..src import app as flask_app, getdistance
+from ..src import app as flask_app, getdistance, getRoute, gasStations
 
-def test_good_route():
+def test_good_distance():
     with flask_app.test_client() as c:
         home = "Stuttgart"
         work = "Böblingen"
@@ -10,3 +10,58 @@ def test_good_route():
         })
         answer = getdistance()
         assert "Stunden" in answer and "Km" in answer
+
+def test_bad_distance():
+    with flask_app.test_client() as c:
+        home = "Stutsdhd"
+        work = "Böblingen"
+        rv = c.post('/distance', json={
+            'homeLocation': home,
+            'workingLocation': work
+        })
+        answer = getdistance()
+        assert "Mindestens eine der Angaben ist kein valider Ort" in answer
+
+def test_bad_route():
+    with flask_app.test_client() as c:
+        home = "Hochdahl"
+        work = "Ratingen"
+        rv = c.post('/route', json={
+            'homeLocation': home,
+            'workingLocation': work
+        })
+        answer = getRoute()
+        assert "folgende" in answer and "Route" in answer
+
+def test_bad_route():
+    with flask_app.test_client() as c:
+        home = "sdhgoisvh"
+        work = "Ratingen"
+        rv = c.post('/route', json={
+            'homeLocation': home,
+            'workingLocation': work
+        })
+        answer = getRoute()
+        assert "Mindestens eine der Angaben ist kein valider Ort" in answer
+
+def test_good_gas():
+    with flask_app.test_client() as c:
+        home = "Stuttgart"
+        fuel = "Benzin"
+        rv = c.post('/distance', json={
+            'homeLocation': home,
+            'gasolineType': fuel
+        })
+        answer = gasStations()
+        assert "Tankstellen" in answer and "Stadt" in answer
+
+def test_bad_gas():
+    with flask_app.test_client() as c:
+        home = "adfv"
+        fuel = "Diesel"
+        rv = c.post('/distance', json={
+            'homeLocation': home,
+            'gasolineType': fuel
+        })
+        answer = gasStations()
+        assert "Kein valider Ort angegeben" in answer
