f1fb5cdaed6d9bca0346a4b61bba0975ae5d9b4a
n1klasD <67094918+n1klasD@users.noreply.github.com>
2022-04-13 18:22:02 +0200

    updated compsoe and start

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ sudo docker pull 1646552/welcome:latest
 sudo docker pull 1646552/entertainment:latest
 sudo docker pull 1646552/journey:latest
 sudo docker pull 1646552/finances:latest
-sudo docker-compose up
+sudo docker-compose up --remove-orphans
